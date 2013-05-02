ecb20e74e34a2101bd308f3904fe56b23f707fcd
ffiadmin <admin@forwardfour.com>
2013-05-02 00:16:42 -0400

    This revision has changed the configuration file to use a CDN.

--- config.php
+++ config.php
@@ -2,13 +2,13 @@
 	namespace FFI\RF;
 
 //Create template-specific global definitions
-	define("FFI\RF\CDN", false);
+	define("FFI\RF\CDN", true);
 	define("FFI\RF\HOME", site_url());
 	define("FFI\RF\PATH", dirname(__FILE__));
 	define("FFI\RF\RESOURCE_PATH", (CDN ? "//ffistatic.appspot.com/sga" : site_url()) . "/wp-content/themes/refined/");
 	
 //Remove the admin bar, the theme will provide one
-	function removeMargin() { // css override for the frontend  
+	function removeMargin() { // CSS override for the frontend portion of the site 
 		echo "<style>html {margin-top: 0px !important;} * html body {margin-top: 0px !important;}</style>
 ";  
     }
